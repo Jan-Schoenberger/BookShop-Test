a3be425270996a5d7a05b22055c9ec50fbaaf329
Jan Schönberger <jan.s19@schule.koeln>
2020-10-27 08:14:41 +0100

    Test

--- BookShop Test/Class1.cs
+++ BookShop Test/Class1.cs
@@ -6,7 +6,7 @@ namespace BookShop_Test
 {
     class Class1
     {
-
+        // Ich teste ob ich mitlerweile GitHub verwenden kann.
         public int Id { get; set; }
         public string FirstName { get; set; }
 
