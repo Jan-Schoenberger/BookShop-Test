8ae1606326b25f885004a328bca0d608499ec766
Jan Schönberger <jan.s19@schule.koeln>
2020-10-27 08:01:30 +0100

    Projektdateien hinzufügen.

---
+++ BookShop Test/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop_Test
+{
+    class Class1
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+
+    }
+}
---
+++ BookShop Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookShop_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
